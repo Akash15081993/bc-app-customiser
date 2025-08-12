c0fe1293f8d1b8508c01ce7e4baf2bcbbfb8c9d6
Akash Kapadiya <akash780288@gmail.com>
2025-08-12 21:52:23 +0530

    Update JS

---
+++ public/scripts/bigcommerce/product.js
@@ -0,0 +1,6 @@
+const kr_storefront_api = "{{settings.storefront_api.token}}";
+const kr_product_id = "{{product.id}}";
+console.warn('kr_storefront_api');
+console.warn(kr_storefront_api);
+console.warn('kr_product_id');
+console.warn(kr_product_id);
