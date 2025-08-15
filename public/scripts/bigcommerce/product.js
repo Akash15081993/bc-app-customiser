66f4c3369c960d72a19a4b7a61ce149d493bf441
Akash Kapadiya <akash780288@gmail.com>
2025-08-15 15:59:11 +0530

    Update product js

--- public/scripts/bigcommerce/product.js
+++ public/scripts/bigcommerce/product.js
@@ -1,7 +1,11 @@
-console.log('krcustomizer_config V3')
-console.log(krcustomizer_config)
+const krAppConfig = window?.krcustomizer_config;
+console.log('krAppConfig');
+console.log(krAppConfig);
 
-const bc_storefront_token = krcustomizer_config?.storefront_api;
+const bc_storefront_token = krAppConfig?.storefront_api;
+const kr_product_id = krAppConfig?.product_id;
+const kr_store_hash = krAppConfig?.store_hash;
+
 const kr_root_app_id = "kr-customizer-root";
 const ele_customize_handel_button = document?.querySelector('body .kr-customize-handel');
 const ele_addtocart_handel_button = document?.querySelector('body .kr-addtocart-handel');
@@ -9,26 +13,35 @@ const ele_product_form = document?.querySelector('.productView-options form');
 let kr_store_form_data = {};
 
 
-document.addEventListener('DOMContentLoaded', function () {
+//Root app model visibility
+function appModelVisibility(action) {
+    if (action === "show") {
+        document.getElementById(kr_root_app_id).style.display = 'block';
+    } else {
+        document.getElementById(kr_root_app_id).style.display = 'none';
+    }
+}
 
+//Mount app
+document.addEventListener('DOMContentLoaded', function () {
     if (!document.getElementById(kr_root_app_id)) {
         document.body.insertAdjacentHTML('beforeend', `<div id="${kr_root_app_id}" style="display:none;position: fixed;top: 0;left:0;width:100%;z-index: 9999999999;"></div>`);
     }
 
     if (typeof window.mountProductCustomizer === 'function') {
         window.mountProductCustomizer(`#${kr_root_app_id}`, {
-            productId: krcustomizer_config?.product_id,
-            storeHash: krcustomizer_config?.store_hash
+            productId: kr_product_id,
+            storeHash: kr_store_hash
         });
     } else {
         console.error('Customizer failed to load');
     }
 });
 
-
+//customize button Validation & handel
 ele_customize_handel_button?.addEventListener("click", function () {
     kr_store_form_data = {};
-    document.getElementById(kr_root_app_id).style.display = 'none';
+    appModelVisibility('hide');
 
     //Validation form
     if (ele_product_form.checkValidity && !ele_product_form.checkValidity()) {
@@ -48,29 +61,81 @@ ele_customize_handel_button?.addEventListener("click", function () {
     // Collect all form data into an object
     const formData = new FormData(ele_product_form);
     formData.forEach(function (value, key) {
-        if (kr_store_form_data[key]) {
-            if (Array.isArray(kr_store_form_data[key])) {
-                kr_store_form_data[key].push(value);
-            } else {
-                kr_store_form_data[key] = [kr_store_form_data[key], value];
-            }
+        value = value?.trim?.() || ""; // remove spaces
+
+        // skip unwanted keys and empty values
+        if (key === "action" || value === "") { return; }
+
+        // Extract attribute id if key is like attribute[170]
+        const match = key.match(/^attribute\[(\d+)\]$/);
+        if (match) {
+            kr_store_form_data[match[1]] = value; // store only the number as key
         } else {
             kr_store_form_data[key] = value;
         }
     });
 
-    document.getElementById(kr_root_app_id).style.display = 'block';
+    appModelVisibility('show');
 
 });
 
 
+//Get product variant id
+async function getProductVariantId(productData) {
+    const responseReq = await fetch("/graphql", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + bc_storefront_token },
+        body: JSON.stringify({ query: `query product{ site{ product(sku:"${productData}"){ variants(skus:["${productData}"]){ edges{ node{ entityId } } } } } } ` })
+    });
+    const resultData = await responseReq?.json();
+    console.log('graphql getProducts')
+    console.log(resultData)
+    if (resultData?.data?.site?.product && resultData?.data?.site?.product?.variants?.edges?.length > 0) {
+        return resultData?.data?.site?.product?.variants?.edges[0]?.node?.entityId;
+    } else {
+        return 0;
+    }
+}
+
+
+//Get product details based on selected options
+async function productWithSelectedOptions(options) {
+    let query_parameters = ``;
+    let optionEntity = ``;
+
+    Object.entries(options).forEach(([key, value]) => {
+        // Skip unwanted keys and empty values
+        if (key === "product_id" || key === "qty[]" || value === "" || isNaN(Number(value))) return;
+        optionEntity += `{ optionEntityId: ${parseInt(key)}, valueEntityId: ${parseInt(value)} },`;
+    });
+
+    query_parameters = `product (entityId: ${parseInt(options.product_id)} optionValueIds: [${optionEntity.slice(0, -1)}] )`;
+
+    const responseReq = await fetch("/graphql", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + bc_storefront_token },
+        body: JSON.stringify({ query: `query ProductsWithOptionSelections { site { productWithSelectedOptions: ${query_parameters} { ...ProductFields } } } fragment ProductFields on Product { sku prices{ price{ value } } }` })
+    });
+    const resultData = await responseReq?.json();
+    const productData = resultData?.data?.site?.productWithSelectedOptions;
+    const sku = productData?.sku;
+    const kr_product_price = productData?.prices?.price?.value;
+    const kr_product_variant = await getProductVariantId(sku);
+
+    const krDesignData = window.localStorage.getItem("krDesignData");
+    return { kr_product_variant, kr_product_price, kr_product_id, kr_store_form_data, kr_store_hash, krDesignData };
+}
+
+
+//Add to Cart Handel
 document.addEventListener("click", async function (e) {
-    const button = e.target.closest('[title="Add design to cart"]');
-    if (button) {
+    console.clear();
+    const addtocartButton = e.target.closest('[title="Add design to cart"]');
+    if (addtocartButton) {
 
         //Validation form
         if (ele_product_form.checkValidity && !ele_product_form.checkValidity()) {
-            document.getElementById(kr_root_app_id).style.display = 'none';
+            appModelVisibility('hide');
             if (ele_product_form.reportValidity) {
                 ele_product_form.reportValidity();
             } else {
@@ -85,23 +150,15 @@ document.addEventListener("click", async function (e) {
         }
 
         if (kr_store_form_data == null || (typeof kr_store_form_data === 'object' && Object.keys(kr_store_form_data).length === 0)) {
-            document.getElementById(kr_root_app_id).style.display = 'none';
+            appModelVisibility('hide');
             return false;
         }
+        appModelVisibility('show');
 
-        document.getElementById(kr_root_app_id).style.display = 'block';
+        const productData = await productWithSelectedOptions(kr_store_form_data);
 
-        console.log('kr_store_form_data', kr_store_form_data);
-
-
-        const fetchData = await fetch("/graphql", {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + bc_storefront_token },
-            body: JSON.stringify({'query': `query CategoryTree3LevelsDeep { site { categoryTree { name path } } }`})
-        });
-        const resultData = await fetchData?.json();
-        console.log('graphql resultData')
-        console.log(resultData)
+        console.log('productData Final');
+        console.log(productData)
 
     }
 });
