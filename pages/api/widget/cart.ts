04f3d59bca82f07bec631a5ad5cdf59b47f9226a
Akash Kapadiya <akash780288@gmail.com>
2025-08-21 15:27:32 +0530

    Add cors 1

--- pages/api/widget/cart.ts
+++ pages/api/widget/cart.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { runCors } from "@lib/cors";
 import { bigcommerceClient } from "@lib/auth";
+import { runCors } from "@lib/cors";
 import { mysqlQuery } from "@lib/dbs/mysql";
 import languageEN from "lang/en";
 
