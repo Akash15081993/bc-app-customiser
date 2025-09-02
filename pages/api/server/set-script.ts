e267b8f4fbdcf6fbd570c122770aadaf825fb37b
Inderpal1000 <Info@kusheldigi.com>
2025-09-02 14:34:37 +0530

    Script manager issue V6

--- pages/api/server/set-script.ts
+++ pages/api/server/set-script.ts
@@ -20,7 +20,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         await bigcommerce.post(`/content/scripts`, scriptPayload);
     }
 
-
     // -------------------------
     // Ensure Order Webhook
     // -------------------------
